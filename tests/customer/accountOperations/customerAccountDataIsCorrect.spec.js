e7e4f0dcd8bba2912b19b3622e93b13f32d64e99
Max Savchenko <savchenkomaks96@gmail.com>
2025-06-22 14:09:35 +0300

    Add tests for WizardBank

--- tests/customer/accountOperations/customerAccountDataIsCorrect.spec.js
+++ tests/customer/accountOperations/customerAccountDataIsCorrect.spec.js
@@ -1,17 +1,17 @@
-import { test } from "@playwright/test";
-import { CustomerLoginPage } from "../../../src/pages/customer/CustomerLoginPage";
-import { CustomerAccountPage } from "../../../src/pages/customer/CustomerAccountPage";
+import { test } from '@playwright/test';
+import { CustomerLoginPage } from '../../../src/pages/customer/CustomerLoginPage';
+import { CustomerAccountPage } from '../../../src/pages/customer/CustomerAccountPage';
 
-test("Assert customer has correct bank data", async ({ page }) => {
+test('Assert customer has correct bank data', async ({ page }) => {
   const loginPage = new CustomerLoginPage(page);
   const accountPage = new CustomerAccountPage(page);
 
   await loginPage.open();
-  await loginPage.selectCustomer("Hermoine Granger");
+  await loginPage.selectCustomer('Hermoine Granger');
   await loginPage.clickLoginButton();
 
-  await accountPage.assertAccountIdInDropDownHasValue("number:1001");
-  await accountPage.assertAccountLineContainsText("Account Number : 1001");
-  await accountPage.assertAccountLineContainsText("Balance : 5096");
-  await accountPage.assertAccountLineContainsText("Currency : Dollar");
+  await accountPage.assertAccountIdInDropDownHasValue('number:1001');
+  await accountPage.assertAccountLineContainsText('Account Number : 1001');
+  await accountPage.assertAccountLineContainsText('Balance : 5096');
+  await accountPage.assertAccountLineContainsText('Currency : Dollar');
 });
